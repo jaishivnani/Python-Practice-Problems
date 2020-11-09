736ebdf8b2339879855b9f14b5af93d023fe7b1b
Jai Shivnani <jaishivnani30@gmail.com>
2020-11-09 23:14:10 +0530

    string more problems

---
+++ 3. Chapter 3 Strings/Practice problems/22_string.py
@@ -0,0 +1,16 @@
+'''Write a Python function to convert a given string to all uppercase
+if it contains at least 2 uppercase characters in the first 4 characters.'''
+
+def to_uppercase(str):
+    num_upper=0
+    for letter in str[0:4]:
+        if letter.upper()==letter:
+            num_upper+=1
+    if num_upper>=2:
+        return str.upper()
+    return str
+
+print(to_uppercase('GeeksforGeeks'))
+
+
+
